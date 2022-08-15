28c0579d12cb3da9252d25b50b48c70d8c9a00c4
Kevin Kash <kevin.kash@tlgcohort.com>
2022-08-15 15:40:09 -0700

    GameWindow changed to HomeWindow. Functionality and graphics added to home window. New GameWindow created.

--- src/com/simiansays/model/app/SimianSaysApp.java
+++ src/com/simiansays/model/app/SimianSaysApp.java
@@ -1,10 +1,28 @@
 package com.simiansays.model.app;
 
 import com.simiansays.model.GameWindow;
+import com.simiansays.model.HomeWindow;
 
 import javax.swing.*;
+import java.awt.event.ActionListener;
 
 public class SimianSaysApp {
+
+    public void executeHomeWindow() {
+        /*
+         * Swing method 'invokeLater' requires as a parameter an instance of runnable. InvokeLater will ensure all swing related
+         * code will run on the same thread. This thread is called, "Event Dispatch Thread (EDT)". Swing is not thread safe, so all
+         * swing code should run through this thread.
+         */
+        SwingUtilities.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                HomeWindow homeWindow = new HomeWindow();
+                homeWindow.show();
+            }
+        });
+    }
+
 
     public void executeGameWindow() {
         /*
@@ -20,5 +38,6 @@ public class SimianSaysApp {
             }
         });
     }
+
 
 }
